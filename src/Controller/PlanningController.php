6a0d8364cec315f6534f8b59ae5d850db46d623b
unknown <DAUJATB@siopergaud.fr>
2020-09-15 13:28:56 +0200

    First commit

---
+++ src/Controller/PlanningController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PlanningController extends AbstractController
+{
+    /**
+     * @Route("/planning", name="planning")
+     */
+    public function planning()
+    {
+        return $this->render('planning/index.html.twig');
+    }
+}
