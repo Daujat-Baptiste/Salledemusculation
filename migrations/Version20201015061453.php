57abc823aceb0538780cb0000f73bfbe1161c9a7
unknown <daujatb@SIOPERGAUD.FR>
2020-10-15 09:44:55 +0200

    lot1css

--- migrations/Version20201015061453.php
+++ migrations/Version20201015061453.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201014220857 extends AbstractMigration
+final class Version20201015061453 extends AbstractMigration
 {
     public function getDescription() : string
     {
