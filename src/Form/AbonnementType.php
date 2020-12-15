f373fdf2482380a2721ad2da4e8a1f0bf0070643
Daujat-Baptiste <baptiste.daujat@sfr.fr>
2020-12-15 09:52:03 +0100

    Rework type abonnement

--- src/Form/AbonnementType.php
+++ src/Form/AbonnementType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Abonnement;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +16,8 @@ class AbonnementType extends AbstractType
             ->add('prix')
             ->add('nom')
             ->add('description')
-            ->add('image')
+            ->add('image', FileType::class, [
+            ])
         ;
     }
 
