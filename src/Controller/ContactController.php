f293b2e365c565bb0c800035cf1ba8c62fb9608c
Daujat-Baptiste <baptiste.daujat@sfr.fr>
2021-01-05 09:48:16 +0100

    Contact back et front

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,16 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactFrontType;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/contact", name="contact", methods={"GET","POST"})
      */
-    public function contact()
+    public function contact(Request $request): Response
     {
-        return $this->render('contact/index.html.twig');
+        $contact = new Contact();
+        $form = $this->createForm(ContactFrontType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'contact' => $contact,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/contact", name="contact_admin", methods={"GET","POST"})
+     */
+    public function listecontact(ContactRepository $contactRepository, Request $request): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+        }
+        return $this->render('contact/contactAdmin.html.twig', [
+            'contacts' => $contactRepository->findAll(),
+            'form'=>$form->createView(),
+        ]);
     }
 }
---
+++ src/Entity/Contact.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ContactRepository::class)
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Contenu;
+
+    /**
+     * @ORM\Column(type="boolean",nullable=true)
+     */
+    private $lu;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->Contenu;
+    }
+
+    public function setContenu(string $Contenu): self
+    {
+        $this->Contenu = $Contenu;
+
+        return $this;
+    }
+
+    public function getLu(): ?bool
+    {
+        return $this->lu;
+    }
+
+    public function setLu(bool $lu): self
+    {
+        $this->lu = $lu;
+
+        return $this;
+    }
+}
