317fb24ada87bbf490e87e8dda861d1adffb7f66
Daujat-Baptiste <baptiste.daujat@sfr.fr>
2021-03-29 21:20:48 +0200

    Mailer contadct fini back

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,16 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\Mail;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use App\Form\MailType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends AbstractController
 {
+    private $mailerInterface;
+    private $security;
+
+    public function __construct(MailerInterface $mailerInterface, Security $security)
+    {
+        $this->mailerInterface = $mailerInterface;
+        $this->security = $security;
+    }
+
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact()
+    public function contact(Request $request, EntityManagerInterface $entityManager)
     {
-        return $this->render('contact/index.html.twig');
+        $mail = new Mail();
+        $form = $this->createForm(MailType::class, $mail);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager();
+            $this->sendMail($mail);
+            $mail->setExpediteur($this->security->getUser()->getUsername());
+            $entityManager->persist($mail);
+            $entityManager->flush();
+            return $this->redirectToRoute('administration');
+        } else {
+            return $this->render('contact/index.html.twig',
+                ['form' => $form->createView(),
+                ]);
+        }
+    }
+
+    public function sendMail(Mail $mail)
+    {
+        $email = (new email())
+            ->from($this->security->getUser()->getUsername())
+            ->to($mail->getDestinataire())
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject($mail->getSubject())
+            //->text('Sending emails is fun again!')
+            ->html($mail->getContenu());
+        $this->mailerInterface->send($email);
     }
 }
---
+++ src/Entity/Mail.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MailRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MailRepository::class)
+ */
+class Mail
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $destinataire;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $expediteur;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $subject;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDestinataire(): ?string
+    {
+        return $this->destinataire;
+    }
+
+    public function setDestinataire(string $destinataire): self
+    {
+        $this->destinataire = $destinataire;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getExpediteur(): ?string
+    {
+        return $this->expediteur;
+    }
+
+    public function setExpediteur(string $expediteur): self
+    {
+        $this->expediteur = $expediteur;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+}
